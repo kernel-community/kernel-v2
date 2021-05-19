fce24ee7162d35ca9a07ee9eaf2de57a762de8b0
andytudhope <tuddy0525@gmail.com>
2021-05-19 18:35:32 +0200

    Update gaming track sidebar ordering and try get around redirect effecting twitter cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,36 @@
-import React, {useEffect} from "react";
-import {Helmet} from "react-helmet";
-import {navigate} from "gatsby";
-import {useTranslation} from "@modules/localization";
-import {getInitialLocale} from "@utils";
+import React, {useEffect} from 'react';
+import {Helmet} from 'react-helmet';
+import {navigate} from 'gatsby';
+import {useTranslation} from '@modules/localization';
+import {getInitialLocale} from '@utils';
+import {SEO} from '@modules/utility';
 
 //This page doesn't exist and solely acts as a reroute for language.
 const IndexPage = () => {
-    const {allLocales, DEFAULT_LOCALE} = useTranslation();
+  const {allLocales, DEFAULT_LOCALE} = useTranslation();
 
-    useEffect(() => {
-        if (typeof window !== "undefined" && window) {
-            let initialLocale = getInitialLocale(allLocales, DEFAULT_LOCALE);
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window) {
+      let initialLocale = getInitialLocale(allLocales, DEFAULT_LOCALE);
 
-            //Replace current route with locale based index.
-            navigate(`/${initialLocale}/`, {replace: true});
-        }
-    });
+      //Replace current route with locale based index.
+      navigate(`/${initialLocale}/`, {replace: true});
+    }
+  });
 
-    return (
-        <Helmet>
-            <meta name='robots' content='noindex, nofollow' />
-        </Helmet>
-    );
+  const seo = {
+    title: 'Kernel Community',
+    description: '/images/kernel_lp.png',
+    keywords: 'community, kernel, contribute',
+    featuredImage: '/images/kernel_lp.png'
+  };
+
+  return (
+    <Helmet>
+      <meta name="robots" content="noindex, nofollow" />
+      <SEO {...seo} />
+    </Helmet>
+  );
 };
 
 export default IndexPage;
