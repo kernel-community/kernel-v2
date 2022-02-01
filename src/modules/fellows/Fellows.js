2f92c1c8752bd7c4b901c0ba4bd724c4cc88a781
malayvasa <malayvasa@gmail.com>
2022-02-01 19:11:28 +0530

    Added Fellows & Projects Component

---
+++ src/modules/fellows/Fellows.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import {Flex, Text, Image, Grid, Button} from 'theme-ui';
+
+const originalArray = require('../../../content/fellows.json');
+
+const filteredFellows = originalArray.fellows.sort((a, b) => {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+});
+
+const Fellows = () => {
+  return (
+    <Flex
+      sx={{
+        marginTop: ['-2rem', '-4rem'],
+        marginBottom: ['4rem', '4rem'],
+        flexDirection: 'column',
+        backgroundColor: 'surfaceAlt',
+        borderRadius: 'medium',
+        marginX: ['2rem', '2rem', '0rem']
+      }}>
+      <Grid
+        id="fellowContainer"
+        gap={0}
+        columns={2}
+        sx={{
+          gridColumnGap: ['2rem', '4rem'],
+          gridRowGap: ['2.5rem', '4rem'],
+          paddingBottom: ['0rem', '0rem'],
+          overflowX: 'scroll',
+          paddingX: '2rem',
+          paddingTop: '2rem',
+          gridTemplateRows: ['auto auto auto', 'auto auto'],
+          gridAutoFlow: 'column',
+          borderRadius: '0.5rem',
+          scrollBehavior: 'smooth'
+        }}>
+        {filteredFellows.map((fellow, index) => (
+          <Flex
+            sx={{
+              flexDirection: 'column',
+              ':hover': {
+                cursor: 'pointer',
+                color: 'primary'
+              },
+
+              minWidth: [100, 100]
+            }}
+            onClick={() => {
+              window.open(fellow.url, '_blank');
+            }}>
+            <Image
+              src={fellow.image}
+              sx={{
+                minWidth: [60, 100],
+                maxWidth: [60, 100],
+                minHeight: [60, 100],
+                maxHeight: [60, 100],
+                borderRadius: '100%',
+                marginX: 'auto',
+                marginBottom: '0.5rem'
+              }}
+            />
+
+            <Text
+              sx={{
+                textAlign: 'center',
+                fontSize: ['0.8rem', '1rem']
+              }}>
+              {fellow.name}
+            </Text>
+          </Flex>
+        ))}
+      </Grid>
+      <Flex
+        sx={{
+          marginX: 'auto',
+          marginBottom: ['0rem', '2rem'],
+          textColor: 'callout',
+          visibility: ['hidden', 'visible']
+        }}>
+        <Button
+          variant="outlineSmall"
+          onClick={() => {
+            document.getElementById('fellowContainer').scrollLeft -= 600;
+          }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            height={24}
+            width={24}>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
+        </Button>
+        <Button
+          variant="outlineSmall"
+          onClick={() => {
+            document.getElementById('fellowContainer').scrollLeft += 600;
+          }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            height={24}
+            width={24}>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            />
+          </svg>
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Fellows;
---
+++ src/modules/projects/Projects.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import {Flex, Text, Image, Grid, Button} from 'theme-ui';
+
+const projectsArray = require('../../../content/projects.json');
+const projects = projectsArray.projects;
+
+const Projects = () => {
+  return (
+    <Flex
+      sx={{
+        backgroundColor: 'surfaceAlt',
+        borderRadius: 'medium',
+        flexDirection: 'column',
+        marginX: ['2rem', '2rem', '0rem'],
+        marginTop: ['-2rem', '-4rem']
+      }}>
+      <Grid
+        id="projectContainer"
+        sx={{
+          gridColumnGap: ['1rem', '4rem'],
+          gridRowGap: ['4rem', '6rem'],
+          padding: '2rem',
+          overflowX: 'scroll',
+          gridTemplateRows: ['auto', 'auto'],
+          gridAutoFlow: 'column',
+          scrollBehavior: 'smooth'
+        }}>
+        {projects.map((project, index) => (
+          <Flex
+            onClick={() => {
+              window.open(project.url, '_blank');
+            }}
+            sx={{
+              flexDirection: 'column',
+              rowGap: '1rem',
+              ':hover': {
+                cursor: 'pointer',
+                color: 'primary'
+              },
+              minWidth: '344px'
+            }}>
+            <Image
+              sx={{borderRadius: '3%', width: '344px', height: '206px'}}
+              src={project.image}
+            />
+            <Text sx={{fontWeight: 600, fontSize: '24px'}}>{project.name}</Text>
+            <Text sx={{color: 'text'}}>{project.description}</Text>
+          </Flex>
+        ))}
+      </Grid>
+      <Flex
+        sx={{
+          marginX: 'auto',
+          marginBottom: ['0rem', '2rem'],
+          textColor: 'callout',
+          visibility: ['hidden', 'visible']
+        }}>
+        <Button
+          variant="outlineSmall"
+          onClick={() => {
+            document.getElementById('projectContainer').scrollLeft -= 600;
+          }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            height={24}
+            width={24}>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
+        </Button>
+        <Button
+          variant="outlineSmall"
+          onClick={() => {
+            document.getElementById('projectContainer').scrollLeft += 600;
+          }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            height={24}
+            width={24}>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            />
+          </svg>
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Projects;
