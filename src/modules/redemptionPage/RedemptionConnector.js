42afced00cfd08eecd4b1e6ea23d718e108ffb74
Anthony M <thestrabusiness@users.noreply.github.com>
2022-05-30 10:20:28 -0400

    Refactor contract calls to use wagmi hooks and integrate notificatins (#362)
    This commit refactors our contract calls to use the wagmi hooks syntax.
    
    Each of our contract calls were also rewritten as hooks to allow us to
    stack the new notification functionality on top of the contract calls.
    
    This lets us easily notifications plug into the `onError` and other
    callback functions given on the wagmi `useContractRead` and
    `useContractWrite` functions.
    
    Another nice benefit of using the contract hooks is that we can avoid
    having saving certain contract calls to state. We can now directly
    use the result of a call in our code instead of having to use `useEffect`
    to wrangle async functions

--- src/modules/redemptionPage/RedemptionConnector.js
+++ src/modules/redemptionPage/RedemptionConnector.js
@@ -1,37 +1,24 @@
 /** @jsx jsx */
 
 import { jsx } from 'theme-ui'
-import { useAccount, useProvider } from 'wagmi'
-import { useEffect, useState } from 'react'
 
-import { hasStakeInCourse } from '@src/course/contracts'
+import { useHasStakeInCourse } from '@src/course/contracts'
 import RegisterButton from './RegisterButton'
 import RedemptionDetails from './RedemptionDetails'
 
-const RedemptionConnector = () => {
-  const { data: accountData } = useAccount()
-  const provider = useProvider()
-  const [hasStakeStakeToRedeem, setHasStakeToRedeem] = useState(false)
+const RedemptionConnector = ({ address }) => {
+  const { data: hasStakeStakeToRedeem, isLoading } = useHasStakeInCourse()
 
-  useEffect(() => {
-    const retrieveAndSetStakeState = async () => {
-      const _hasStake = await hasStakeInCourse(provider)
-      setHasStakeToRedeem(_hasStake)
-    }
-
-    if (accountData?.address && provider) {
-      retrieveAndSetStakeState()
-    }
-  }, [accountData?.address, provider])
-
-  return (
-    <div>
-      {hasStakeStakeToRedeem && (
-        <RedemptionDetails address={accountData?.address} />
-      )}
-      {!hasStakeStakeToRedeem && <RegisterButton />}
-    </div>
-  )
+  if (isLoading) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div>
+        {hasStakeStakeToRedeem && <RedemptionDetails address={address} />}
+        {!hasStakeStakeToRedeem && <RegisterButton />}
+      </div>
+    )
+  }
 }
 
 export default RedemptionConnector
