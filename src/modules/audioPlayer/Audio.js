fce7764f9c1407ab8832e9c79d7b1976e9d2996d
Anthony M <anthony.moffa@thoughtbot.com>
2022-06-21 13:32:56 -0400

    Support multiple AudioPlayer components per page
    This commit enables multiple AudioPlayer instances for a given page.
    Internally, the audio player code was referring to DOM elements by
    static IDs, and so any additional audio players added to the page would
    end up pointing at the first components DOM elements.
    
    The fix here adds a UUID generation library to generate a UUID and
    append it to the various DOM IDs that we use for handling audio-related
    events.

--- src/modules/audioPlayer/Audio.js
+++ src/modules/audioPlayer/Audio.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Audio = ({ src }) => {
+const Audio = ({ src, playerId }) => {
   return (
-    <audio id="audio">
+    <audio id={playerId}>
       <source src={src} />
       Your browser does not support the <code>audio</code> element.
     </audio>
