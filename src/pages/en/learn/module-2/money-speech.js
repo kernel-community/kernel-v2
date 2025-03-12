bb0eb15dc92e9b4d8deaa16c5dd9e99946437c4c
andytudhope <13001517+andytudhope@users.noreply.github.com>
2025-03-12 21:59:17 +0200

    Additional info for #685

--- src/pages/en/learn/module-2/money-speech.js
+++ src/pages/en/learn/module-2/money-speech.js
@@ -71,6 +71,10 @@ const PageMoneySpeech = () => {
         literally make money in ways which benefit the people we each care
         about.
       </p>
+      <p>
+        Please make sure that your browser wallet is connected to Optimism for this 
+        play to go on.
+      </p>
       <div sx={styles.connector}>
         {isConnected && <HonourConnector address={accountData?.address} />}
         {!isConnected && <ConnectButton />}
